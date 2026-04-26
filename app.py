dfdde8a081c36a3ffbdc0289d074805560ff5f23
kiki-glow <kinyaglory296@gmail.com>
2026-04-26 10:48:07 +0300

    fix: port conflicts

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import random
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def generate():
     return jsonify({"base": base, "side": side})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
